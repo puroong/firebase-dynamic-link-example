16215707e0d85e29fb5095b1f7c31509a3174cd9
nostalgia <skfk0135@gmail.com>
2020-11-17 15:31:19 +0900

    feat: 딥링크 처리 구현

--- app/src/main/java/com/example/deeplink/ViewActivity.kt
+++ app/src/main/java/com/example/deeplink/ViewActivity.kt
@@ -1,11 +1,39 @@
 package com.example.deeplink
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.dynamiclinks.ktx.dynamicLinks
+import com.google.firebase.ktx.Firebase
 
 class ViewActivity : AppCompatActivity() {
+    companion object {
+        val TAG = "ViewActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view)
+
+        Firebase.dynamicLinks
+            .getDynamicLink(intent)
+            .addOnSuccessListener(this) { pendingDynamicLinkData ->  
+                var deeplink: Uri? = null
+                if(pendingDynamicLinkData != null) {
+                    deeplink = pendingDynamicLinkData.link
+                }
+
+                if(deeplink != null) {
+                    val tvDeepLinkContent = findViewById<TextView>(R.id.tv_deeplink_content)
+                    tvDeepLinkContent.text = deeplink.toString()
+                }
+                else {
+                    Log.d(TAG, "getDynamicLink: no link found")
+                }
+            }
+            .addOnFailureListener(this) { e -> Log.w(TAG, "getDynamicLink:onFailure", e) }
     }
 }
